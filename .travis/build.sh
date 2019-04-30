f6aedf2c9e549f45fefa26f97a6ee144e4bcef19
Curtis Rueden <ctrueden@wisc.edu>
2019-04-30 11:42:32 -0500

    Update Travis CI configuration
    Hopefully this fixes the invalid encrypted credentials.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh $encrypted_f7babe57c60d_key $encrypted_f7babe57c60d_iv
+sh travis-build.sh $encrypted_2197305b7a12_key $encrypted_2197305b7a12_iv
